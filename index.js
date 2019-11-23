71704c3f393cc3eda8141f9cce8e12bf0ea7d43e
anik <woshikuzzaman@gmail.com>
2019-11-24 00:00:53 +0600

    #Test

--- index.js
+++ index.js
@@ -1,123 +1,30 @@
-"use strict";
-
-const dns = require('dns')
-const net = require('net')
-const Joi = require('@hapi/joi')
-
-let email = "woshikuzzaman@gmail.com"
-let stage = 0,
-    success = false,
-    response = '',
-    completed = false,
-    ended = false,
-    tryagain = false,
-    banner = '',
-    socket = new net.Socket()
-
-const schema = Joi.object({
-    email: Joi.string().trim().email()
-})
-
-const validateResult = schema.validate({
-    email: email,
-})
-
-if (!validateResult.error) {
-    let email = validateResult.value.email
-    let domain = email.split("@")[1]
-
-    dns.resolve(domain, 'MX', (err, records) => {
-        if (err) {
-            console.log('invalid email')
-        } else {
-            records.sort((a, b) => a.priority - b.priority)
-
-            socket.connect({
-                port: 25,
-                host: records[0].exchange
-            }, () => {
-                console.log('Connected')
-            })
-        }
-    })
-} else {
-    console.log('invalid email')
-}
-
-socket.on('data', function(data) {
-    response += data.toString();
-    completed = response.slice(-1) === '\n';
-    if (completed) {
-        console.log(response)
-        switch (stage) {
-            case 0:
-                if (response.indexOf('220') > -1 && !ended) {
-                    // Connection Worked
-                    banner = response
-                    var cmd = 'EHLO ' +  + '\r\n'
-                    console.log(cmd)
-                    socket.write(cmd, function() {
-                        stage++;
-                        response = '';
-                    });
-                } else {
-                    if (response.indexOf('421') > -1 || response.indexOf('450') > -1 || response.indexOf('451') > -1)
-                        tryagain = true;
-                    socket.end();
-                }
-                break;
-            case 1:
-                if (response.indexOf('250') > -1 && !ended) {
-                    // Connection Worked
-                    var cmd = 'MAIL FROM:<' + email + '>\r\n'
-                    console.log(cmd)
-                    socket.write(cmd, function() {
-                        stage++;
-                        response = '';
-                    });
-                } else {
-                    socket.end();
-                }
-                break;
-            case 2:
-                if (response.indexOf('250') > -1 && !ended) {
-                    // MAIL Worked
-                    var cmd = 'RCPT TO:<' + email + '>\r\n'
-                    console.log(cmd)
-                    socket.write(cmd, function() {
-                        stage++;
-                        response = '';
-                    });
-                } else {
-                    socket.end();
-                }
-                break;
-            case 3:
-                if (response.indexOf('250') > -1 && !ended) {
-                    // RCPT Worked
-                    success = true;
-                    console.log('mail verifyed')
-                }
-                stage++;
-                response = '';
-                // close the connection cleanly.
-                if (!ended) {
-                    var cmd = 'QUIT\r\n'
-                    console.log(cmd)
-                    socket.write(cmd);
-                }
-                break;
-            case 4:
-                socket.end();
-        }
-
-    }
-})
-
-socket.once('error', function(err) {
-    console.log("Connection error")
-})
-
-socket.once('end', function() {
-    console.log("Closing connection")
-})
+var verifier = require('email-verify');
+var infoCodes = verifier.infoCodes;
+ 
+verifier.verify( 'mercomatic1953@yahoo.com', function( err, info ){
+  if( err ) console.log(err);
+  else{
+    console.log( "Success (T/F): " + info.success );
+    console.log( "Info: " + info.info );
+ 
+    //Info object returns a code which representing a state of validation:
+ 
+    //Connected to SMTP server and finished email verification
+    console.log(info.code === infoCodes.finishedVerification);
+ 
+    //Domain not found
+    console.log(info.code === infoCodes.domainNotFound);
+ 
+    //Email is not valid
+    console.log(info.code === infoCodes.invalidEmailStructure);
+ 
+    //No MX record in domain name
+    console.log(info.code === infoCodes.noMxRecords);
+ 
+    //SMTP connection timeout
+    console.log(info.code === infoCodes.SMTPConnectionTimeout);
+ 
+    //SMTP connection error
+    console.log(info.code === infoCodes.SMTPConnectionError)
+  }
+});
