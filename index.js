acce0b3f1163df15a02c0495e783cd3794d6db69
anik <woshikuzzaman@gmail.com>
2019-11-23 23:05:32 +0600

    #update

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const net = require('net')
 const Joi = require('@hapi/joi')
 
 let email = "woshikuzzaman@gmail.com"
-let socket = null
+let stage = 0,
+    success = false,
+    response = '',
+    completed = false,
+    ended = false,
+    tryagain = false,
+    banner = '',
+    socket = new net.Socket()
 
 const schema = Joi.object({
     email: Joi.string().trim().email()
@@ -16,8 +23,8 @@ const validateResult = schema.validate({
 })
 
 if (!validateResult.error) {
-	let email = validateResult.value.email
-	let domain = email.split("@")[1]
+    let email = validateResult.value.email
+    let domain = email.split("@")[1]
 
     dns.resolve(domain, 'MX', (err, records) => {
         if (err) {
@@ -25,44 +32,92 @@ if (!validateResult.error) {
         } else {
             records.sort((a, b) => a.priority - b.priority)
 
-            socket = new net.Socket()
-
             socket.connect({
                 port: 25,
                 host: records[0].exchange
             }, () => {
-                console.log('connection open successfully')
-            })
-
-            socket.on('data', (mes) => {
-                console.log(mes.toString())
-            })
-
-            socket.on('connect', () => {
-                console.log('successfully connected with the server')
-            })
-
-            socket.on('ready', () => {
-            	console.log('connection ready')
-            })
-
-            socket.on('error', err => {
-                console.log(err)
-            })
-
-            socket.on('end', err => {
-                console.log('connection end')
-            })
-
-            socket.on('close', err => {
-                console.log('connection close')
-            })
-
-            socket.on('drain', err => {
-                console.log('stream empty')
+                console.log('Connected')
             })
         }
     })
 } else {
     console.log('invalid email')
 }
+
+socket.on('data', function(data) {
+    response += data.toString();
+    completed = response.slice(-1) === '\n';
+    if (completed) {
+        console.log(response)
+        switch (stage) {
+            case 0:
+                if (response.indexOf('220') > -1 && !ended) {
+                    // Connection Worked
+                    banner = response
+                    var cmd = 'EHLO ' +  + '\r\n'
+                    console.log(cmd)
+                    socket.write(cmd, function() {
+                        stage++;
+                        response = '';
+                    });
+                } else {
+                    if (response.indexOf('421') > -1 || response.indexOf('450') > -1 || response.indexOf('451') > -1)
+                        tryagain = true;
+                    socket.end();
+                }
+                break;
+            case 1:
+                if (response.indexOf('250') > -1 && !ended) {
+                    // Connection Worked
+                    var cmd = 'MAIL FROM:<' + email + '>\r\n'
+                    console.log(cmd)
+                    socket.write(cmd, function() {
+                        stage++;
+                        response = '';
+                    });
+                } else {
+                    socket.end();
+                }
+                break;
+            case 2:
+                if (response.indexOf('250') > -1 && !ended) {
+                    // MAIL Worked
+                    var cmd = 'RCPT TO:<' + email + '>\r\n'
+                    console.log(cmd)
+                    socket.write(cmd, function() {
+                        stage++;
+                        response = '';
+                    });
+                } else {
+                    socket.end();
+                }
+                break;
+            case 3:
+                if (response.indexOf('250') > -1 && !ended) {
+                    // RCPT Worked
+                    success = true;
+                    console.log('mail verifyed')
+                }
+                stage++;
+                response = '';
+                // close the connection cleanly.
+                if (!ended) {
+                    var cmd = 'QUIT\r\n'
+                    console.log(cmd)
+                    socket.write(cmd);
+                }
+                break;
+            case 4:
+                socket.end();
+        }
+
+    }
+})
+
+socket.once('error', function(err) {
+    console.log("Connection error")
+})
+
+socket.once('end', function() {
+    console.log("Closing connection")
+})
